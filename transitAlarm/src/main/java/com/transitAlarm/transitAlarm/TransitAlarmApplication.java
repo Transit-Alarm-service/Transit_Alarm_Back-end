1958730715256c0ddda58f5d2e081e28b6cb8ace
CHO Min Ho <chominho96@inha.edu>
2022-04-03 22:18:54 +0900

    Add Spring Project File
    1. add spring project file

---
+++ transitAlarm/src/main/java/com/transitAlarm/transitAlarm/TransitAlarmApplication.java
@@ -0,0 +1,13 @@
+package com.transitAlarm.transitAlarm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransitAlarmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransitAlarmApplication.class, args);
+	}
+
+}
---
+++ transitAlarm/src/test/java/com/transitAlarm/transitAlarm/TransitAlarmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.transitAlarm.transitAlarm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransitAlarmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
