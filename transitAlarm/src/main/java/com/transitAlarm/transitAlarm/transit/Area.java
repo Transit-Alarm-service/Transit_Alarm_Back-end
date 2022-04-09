2e56da4e7ca7d80167b4d97f28899103da723a6a
kth1103 <johan1103@naver.com>
2022-04-09 17:13:44 +0900

    Add seoulBusController

--- transitAlarm/src/main/java/com/transitAlarm/transitAlarm/transit/Area.java
+++ transitAlarm/src/main/java/com/transitAlarm/transitAlarm/transit/Area.java
@@ -2,7 +2,6 @@ package com.transitAlarm.transitAlarm.transit;
 
 public enum Area {
     SEOUL,
-    GYEONGGI,
-
+    GYEONGGI
 
 }
--- transitAlarm/src/main/java/com/transitAlarm/transitAlarm/transit/Transit.java
+++ transitAlarm/src/main/java/com/transitAlarm/transitAlarm/transit/Transit.java
@@ -4,19 +4,19 @@ import lombok.extern.java.Log;
 
 import javax.persistence.*;
 
-@Entity
-@DiscriminatorValue("dtype")
+//@Entity
+//@DiscriminatorValue("dtype")
 public class Transit {
 
-    @Id
-    @GeneratedValue
-    @Column(name = "transit_id")
+//    @Id
+//    @GeneratedValue
+//    @Column(name = "transit_id")
     private Long Id;
 
-    @Column(name = "transit_name")
+//    @Column(name = "transit_name")
     private String name;
 
-    @Column(name = "transit_area")
+//    @Column(name = "transit_area")
     private Area area;
 
 
